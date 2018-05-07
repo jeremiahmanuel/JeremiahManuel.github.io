228f5ab305693cb39ba4de89a68b0372e58ebac6
Jeremiah Manuel <jeremiahthaiparambil@gmail.com>
2018-05-07 21:38:14 +0530

    jQuery being developed

--- pp1.js
+++ pp1.js
@@ -1,27 +1,11 @@
-/*//Automatic Quotes Slideshow
 
-let qIndex = 0;
-let q=["laziness is the mother of all bad habits yet she is a mother and we should Respect her",
-"It’s foolish to fear what we’ve yet to see and know"];
-let a=["shikamaru nara","Masashi Kishimoto"];
-
-let quote = function (qIndex) {
-    qIndex++;
-    if (qIndex !< q.length ) {
-     qIndex = 0;
-    }
-    $(".blockquote").text(q[qIndex]);
-    $(".blockquote").children().text(a[qIndex]);
-    setTimeout(quote(qIndex), 2000); // Change quote every 2 seconds
-}
-quote(0);*/
 
 
 let slideIndex = 1;
 let showSlides=function(n) {
-  var i;
-  var slides = $(".mySlides");
-  var dots = $(".dot");
+  let i;
+  let slides = $(".mySlides");
+  let dots = $(".dot");
   if (n > slides.length)
   {slideIndex = 1;}
   if (n < 1)
@@ -34,7 +18,7 @@ let showSlides=function(n) {
   }
   slides[slideIndex-1].style.display = "inline";
   dots[slideIndex-1].className += " active";
-}
+};
 
 showSlides(slideIndex);
 
---
+++ quote.js
@@ -0,0 +1,16 @@
+//Automatic Quotes Slideshow
+
+let qIndex = 0;
+let q=["laziness is the mother of all bad habits yet she is a mother and we should Respect her","It’s foolish to fear what we’ve yet to see and know"];
+let a=["shikamaru nara","Masashi Kishimoto"];
+let quote = function (qIndex) {
+    if (qIndex >= q.length ) {
+     return 0;
+    }
+    let b = $(".blockquote");
+    b.html("<blockquote class='blockquote' style='font-size:26px;'>\""+q[qIndex]+"\"<footer class='blockquote-footer'>"+a[qIndex]+"</footer></blockquote>");
+    qIndex++;
+    setTimeout(quote(qIndex), 500000); // Change quote every 2 seconds
+};
+
+//quote(0);
