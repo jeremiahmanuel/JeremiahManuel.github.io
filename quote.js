b07602f3ea0dd8e02eecdc3c23f162a8b6706ee2
Jeremiah Manuel <jeremiahthaiparambil@gmail.com>
2018-07-08 23:53:52 +0530

    quote
    the quote prob is fixed and need to add data to it

--- quote.js
+++ quote.js
@@ -3,14 +3,15 @@
 let qIndex = 0;
 let q=["laziness is the mother of all bad habits yet she is a mother and we should Respect her","It’s foolish to fear what we’ve yet to see and know"];
 let a=["shikamaru nara","Masashi Kishimoto"];
-let quote = function (qIndex) {
+function quote() {
     if (qIndex >= q.length ) {
-     return 0;
-    }
+      qIndex=0;
+     //return 0;
+   }
     let b = $(".blockquote");
+    console.log(q[qIndex]);
     b.html("<blockquote class='blockquote' style='font-size:26px;'>\""+q[qIndex]+"\"<footer class='blockquote-footer'>"+a[qIndex]+"</footer></blockquote>");
-    qIndex++;
-    setTimeout(quote(qIndex), 5000); // Change quote every 2 seconds
-};
-
+    qIndex=qIndex+1;
+}
+setInterval(quote,3000); // Change quote every 2 seconds
 //quote(0);
