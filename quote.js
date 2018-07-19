27bf1b0982f8e2e0c6c725cc708aeaf46ec63348
Jeremiah Manuel <jeremiahthaiparambil@gmail.com>
2018-07-19 15:35:42 +0530

    add on

--- quote.js
+++ quote.js
@@ -14,8 +14,8 @@ function quote() {
    }
     let b = $(".blockquote");
   //  console.log(q[qIndex]);
-    b.html("<blockquote class='blockquote' style='font-size:26px;'>\""+q[qIndex]+"\"<footer class='blockquote-footer'>"+a[qIndex]+"</footer></blockquote>");
+    b.html("<blockquote class='blockquote fade1' style='font-size:26px; transition:background-color 15s ease'>\""+q[qIndex]+"\"<footer class='blockquote-footer'>"+a[qIndex]+"</footer></blockquote>");
     qIndex=qIndex+1;
 }
-setInterval(quote,3000); // Change quote every 2 seconds
+setInterval(quote,5000); // Change quote every 2 seconds
 //quote(0);
